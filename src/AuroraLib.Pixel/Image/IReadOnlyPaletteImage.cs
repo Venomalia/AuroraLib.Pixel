09333f6c9170b85658343ae5379d9f76d4fea050
Venomalia <87765258+Venomalia@users.noreply.github.com>
2026-07-28 20:23:31 +0200

    Add non-generic IReadOnlyPaletteImage

---
+++ src/AuroraLib.Pixel/Image/IReadOnlyPaletteImage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AuroraLib.Pixel.Image
+{
+
+    /// <summary>
+    /// Represents a read-only indexed image with a color palette.
+    /// </summary>
+    public interface IReadOnlyPaletteImage : IReadOnlyImage
+    {
+        /// <summary>
+        /// Gets the reference count for each palette entry.
+        /// A value of 0 indicates that the corresponding palette index is not currently used by any pixel.
+        /// </summary>
+        ReadOnlySpan<int> PaletteRefCounts { get; }
+
+        /// <summary>
+        /// Retrieves the index of the color in the palette for the pixel at the specified position (x, y).
+        /// </summary>
+        /// <param name="x">The horizontal coordinate of the pixel.</param>
+        /// <param name="y">The vertical coordinate of the pixel.</param>
+        /// <returns>The index in the palette corresponding to the pixel at the specified coordinates.</returns>
+        int GetPixelIndex(int x, int y);
+
+        /// <summary>
+        /// Returns the internal pixel buffer.
+        /// </summary>
+        /// <returns>An untyped <see cref="IReadOnlyImage"/> representing the raw pxel buffer of the image.</returns>
+        IReadOnlyImage GetBuffer();
+    }
+}
--- src/AuroraLib.Pixel/Image/IReadOnlyPaletteImage{TColor}.cs
+++ src/AuroraLib.Pixel/Image/IReadOnlyPaletteImage{TColor}.cs
@@ -6,31 +6,11 @@ namespace AuroraLib.Pixel.Image
     /// Represents a read-only indexed image with a color palette.
     /// </summary>
     /// <typeparam name="TColor">The actual color type stored in the palette.</typeparam>
-    public interface IReadOnlyPaletteImage<TColor> : IReadOnlyImage<TColor> where TColor : unmanaged, IColor<TColor>
+    public interface IReadOnlyPaletteImage<TColor> : IReadOnlyImage<TColor>, IReadOnlyPaletteImage where TColor : unmanaged, IColor<TColor>
     {
         /// <summary>
         /// Gets a read-only list of <typeparamref name="TColor"/> representing the palette.
         /// </summary>
         ReadOnlySpan<TColor> Palette { get; }
-
-        /// <summary>
-        /// Gets the reference count for each palette entry.
-        /// A value of 0 indicates that the corresponding palette index is not currently used by any pixel.
-        /// </summary>
-        ReadOnlySpan<int> PaletteRefCounts { get; }
-
-        /// <summary>
-        /// Retrieves the index of the color in the palette for the pixel at the specified position (x, y).
-        /// </summary>
-        /// <param name="x">The horizontal coordinate of the pixel.</param>
-        /// <param name="y">The vertical coordinate of the pixel.</param>
-        /// <returns>The index in the palette corresponding to the pixel at the specified coordinates.</returns>
-        int GetPixelIndex(int x, int y);
-
-        /// <summary>
-        /// Returns the internal pixel buffer.
-        /// </summary>
-        /// <returns>An untyped <see cref="IReadOnlyImage"/> representing the raw pxel buffer of the image.</returns>
-        IReadOnlyImage GetBuffer();
     }
 }
