9edbb6e71e1f5e23dca305b73892ab89ee20ddda
Venomalia <87765258+Venomalia@users.noreply.github.com>
2026-08-18 18:46:02 +0200

    Make FlatTexture mipmap levels read-only

--- src/AuroraLib.Pixel/Texture/FlatTexture.cs
+++ src/AuroraLib.Pixel/Texture/FlatTexture.cs
@@ -14,19 +14,21 @@ namespace AuroraLib.Pixel.Texture
     /// </summary>
     public sealed class FlatTexture<TColor> : Texture<TColor> where TColor : unmanaged, IColor<TColor>
     {
+        private readonly List<IImage<TColor>> _levels;
+
         /// <summary>
         /// Gets the collection of all mipmap levels for this texture, where index 0 is the base level.
         /// </summary>
-        public List<IImage<TColor>> Levels { get; }
+        public IReadOnlyList<IImage<TColor>> Levels => _levels;
 
         /// <inheritdoc/>
-        public override int LevelCount => Levels.Count;
+        public override int LevelCount => _levels.Count;
 
 
         /// <inheritdoc/>
         public override int MipMapCount
         {
-            get => Levels.Count - 1;
+            get => _levels.Count - 1;
             set
             {
                 if (value < 0)
@@ -37,17 +39,17 @@ namespace AuroraLib.Pixel.Texture
 
                 if (value < MipMapCount)
                 {
-                    for (int i = Levels.Count - 1; i > value; i--)
+                    for (int i = _levels.Count - 1; i > value; i--)
                     {
-                        Levels[i].Dispose();
-                        Levels.RemoveAt(i);
+                        _levels[i].Dispose();
+                        _levels.RemoveAt(i);
                     }
                 }
                 else
                 {
-                    for (int i = Levels.Count; i < value + 1; i++)
+                    for (int i = _levels.Count; i < value + 1; i++)
                     {
-                        var last = Levels[Levels.Count - 1];
+                        var last = _levels[_levels.Count - 1];
 
                         if (last.Width == 1 || last.Height == 1)
                             break;
@@ -65,7 +67,7 @@ namespace AuroraLib.Pixel.Texture
                         }
 
                         mip.ResizeFrom(last, Resamplers.Box);
-                        Levels.Add(mip);
+                        _levels.Add(mip);
                     }
                 }
             }
@@ -73,37 +75,73 @@ namespace AuroraLib.Pixel.Texture
 
         public FlatTexture(IEnumerable<IImage<TColor>> levels)
         {
-            Levels = new List<IImage<TColor>>(levels);
-            if (Levels.Count == 0)
+            _levels = new List<IImage<TColor>>(levels);
+            if (_levels.Count == 0)
                 throw new ArgumentException("A texture must contain at least one level.");
         }
 
         public FlatTexture(IImage<TColor> baseLevel, int mipLevel = 0)
         {
-            Levels = new List<IImage<TColor>>() { baseLevel };
+            if (baseLevel is FlatTexture<TColor> tex)
+                _levels = new List<IImage<TColor>>(tex._levels);
+            else if (baseLevel is Texture<TColor>)
+                throw new NotSupportedException();
+            else
+                _levels = new List<IImage<TColor>>() { baseLevel };
+
             MipMapCount = mipLevel;
         }
 
         /// <inheritdoc/>
-        public override IImage<TColor> GetLevel(int index) => Levels[index];
+        public override IImage<TColor> GetLevel(int index) => _levels[index];
 
         /// <inheritdoc/>
         public override void Clear()
         {
-            Levels[0].Clear();
-            for (int i = 1; i < Levels.Count; i++)
+            _levels[0].Clear();
+            for (int i = 1; i < _levels.Count; i++)
             {
-                Levels[i].Dispose();
+                _levels[i].Dispose();
             }
-            Levels.RemoveRange(1, Levels.Count - 1);
+            _levels.RemoveRange(1, _levels.Count - 1);
         }
 
         /// <inheritdoc/>
         public override IImage<TColor1> CloneAs<TColor1>(Rectangle region)
-            => new FlatTexture<TColor1>(Levels.Select((level, index) => level.CloneAs<TColor1>(ScaleMipRegion(region, index))));
+            => new FlatTexture<TColor1>(_levels.Select((level, index) => level.CloneAs<TColor1>(ScaleMipRegion(region, index))));
 
         /// <inheritdoc/>
-        public override IImage Create(int width, int height) => new FlatTexture<TColor>((IImage<TColor>)Levels[0].Create(width, height));
+        public override IImage Create(int width, int height) => new FlatTexture<TColor>((IImage<TColor>)_levels[0].Create(width, height));
+
+        /// <summary>
+        /// Adds the next mipmap level to the texture.
+        /// The image must have the expected dimensions and use the same image type as the base level.
+        /// </summary>
+        /// <param name="map">The image to add as the next mipmap level.</param>
+        public void Add(IImage<TColor> map)
+        {
+            int width = Width >> _levels.Count;
+            int height = Height >> _levels.Count;
+
+            if (map == null)
+                throw new ArgumentNullException(nameof(map));
+
+            if (width == 0 || height == 0)
+                throw new InvalidOperationException("The texture has reached its maximum mipmap level.");
+
+            if (map.Width != width || map.Height != height)
+                throw new ArgumentException($"The mipmap must have a size of {width}x{height}.", nameof(map));
+
+            if (map.GetType() != _levels[0].GetType())
+                throw new ArgumentException("The mipmap must use the same image type as the base level.", nameof(map));
+
+            if (map is IPaletteImage<TColor> pmap && _levels[0] is IPaletteImage<TColor> pbase)
+            {
+                pmap.Palette = pbase.Palette;
+                pmap.Quantizer = new NearestPaletteColorPicker<TColor>();
+            }
+            _levels.Add(map);
+        }
 
         private static Rectangle ScaleMipRegion(Rectangle region, int level)
         {
